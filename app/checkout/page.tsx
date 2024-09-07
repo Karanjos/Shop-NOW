b927f56b05fad55ba3ae346bd56490ee97598bf0
Karanjos <85048256+Karanjos@users.noreply.github.com>
2024-09-08 00:18:53 +0530

    fixed bugs

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,13 +1,16 @@
 import Container from "../components/Container";
 import FormWrap from "../components/FormWrap";
 import CheckOutClient from "./CheckOutClient";
+import { Suspense } from "react";
 
 const Checkout = () => {
   return (
     <div className="p-8">
       <Container>
         <FormWrap>
-          <CheckOutClient />
+          <Suspense fallback={<div>Loading...</div>}>
+            <CheckOutClient />
+          </Suspense>
         </FormWrap>
       </Container>
     </div>
